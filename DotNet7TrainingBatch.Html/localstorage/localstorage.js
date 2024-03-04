e4cce9c911d0b17ed74824fa63966ca8fe267812
Ast_Zinmoag <zinmoagdev@gmail.com>
2024-03-04 17:36:12 +0630

    js plugin

--- DotNet7TrainingBatch.Html/localstorage/localstorage.js
+++ DotNet7TrainingBatch.Html/localstorage/localstorage.js
@@ -1,20 +1,57 @@
+let _blogId = null;
+
+
 function run(){
-    // read();
+    read();
     // edit("b857368c-3a30-41ea-a101-acda9eb2fb1f");
     // create("title", "author", "content");
     // destroy("4ce7a607-ae9a-41dd-8237-66ef022591d3");
     // update("c9d27196-531a-43a8-8345-54298728dcc8", "heloo", "eheee", "thsi is ehe;x");
 }
 
+function generateBlogDatas (count){
+    for(let i = 1; i <= count; i++){
+        create(`title-${i}`, `author-${i}`, `content-${i}`);
+    }
+}
+// generateBlogDatas(100);
+
+function successMessage(message){
+    Notiflix.Notify.success(message);
+}
+
 function read(){
+    console.log("thisis read")
+    if ( $.fn.DataTable.isDataTable('#datatable') ) {
+        $('#datatable').DataTable().destroy();
+    }
+
     let lstBlogs = getblogs();
+    $("#table_data").html("");
+
+    let htmlrow = "";
 
     for(let i = 0 ; i < lstBlogs.length ; i++){
         const item = lstBlogs[i];
         console.log(item.title);
         console.log(item.author);
         console.log(item.content);
+
+        htmlrow += `
+            <tr>
+                <td>
+                    <button type="button" class="btn btn-primary" onclick="edit('${item.id}')">edit</button>
+                    <button type="button" class="btn btn-danger" onclick="destroy('${item.id}')">delete</button>
+                </td>
+                <td>${item.title}</td>
+                <td>${item.author}</td>
+                <td>${item.content}</td>
+            </tr>
+        `
     }
+
+    $("#table_data").html(htmlrow);
+    new DataTable('#datatable');
 }
 
 function edit (id){
@@ -28,53 +65,112 @@ function edit (id){
     }
     
     const item = items[0];
-    console.log(item)
+    
+    $("#title").val(item.title);
+    $("#author").val(item.author);
+    $("#content").val(item.content);
+
+    _blogId = item.id;
 }
 
 function create(title, author, content){
 
-    let lstBlogs = getblogs();
+    Notiflix.Loading.circle();
 
-   const blog = {
-        id : uuidv4(),
-        title : title,
-        author : author,
-        content : content,
-   }
+    setTimeout(() => {
+        let lstBlogs = getblogs();
+    
+       const blog = {
+            id : uuidv4(),
+            title : title,
+            author : author,
+            content : content,
+       }
+    
+       lstBlogs.push(blog);
+       setBlogs(lstBlogs);
 
-   lstBlogs.push(blog);
-   setBlogs(lstBlogs);
+       Notiflix.Loading.remove();
+       successMessage("Blog have been saved Successfully")
+    }, 3000);
+
 }
 
 function update(id, title , author, content){
-    let lstBlogs = getblogs();
 
-    let itemIndex = lstBlogs.findIndex( x => x.id === id);
+    Notiflix.Loading.circle();
 
-    if(itemIndex < 0){
-        console.log("no item fund")
-        return;
-    }
+    setTimeout(() => {
+        
+        let lstBlogs = getblogs();
+    
+        let itemIndex = lstBlogs.findIndex( x => x.id === id);
+    
+        if(itemIndex < 0){
+            console.log("no item fund")
+            return;
+        }
+    
+        const blog = {
+            id : lstBlogs[itemIndex].id,
+            title : title,
+            author : author,
+            content : content,
+       }
+    
+        lstBlogs[itemIndex] = blog;
+    
+        setBlogs(lstBlogs);
+        Notiflix.Loading.remove();
 
-    const blog = {
-        id : lstBlogs[itemIndex].id,
-        title : title,
-        author : author,
-        content : content,
-   }
+        _blogId = null;
+        successMessage("successfullly updated")
 
-   console.log(blog)
-
-    lstBlogs[itemIndex] = blog;
-
-    setBlogs(lstBlogs);
+    }, 3000);
 }
 
+run();
+
 function destroy(id){
+
+    // Swal.fire({
+    //     title: "Are you sure?",
+    //     text: "You won't be able to revert this!",
+    //     icon: "question",
+    //     showCancelButton: true,
+    //     confirmButtonColor: "#3085d6",
+    //     cancelButtonColor: "#d33",
+    //     confirmButtonText: "Yes, delete it!"
+    //   }).then((result) => {
+    //     if (result.isConfirmed) {
+    //         deleteBlog();
+
+    //         successMessage("deleted")
+    //     }
+    //   });
+
+    Notiflix.Confirm.show(
+        'Are You sure about that',
+        'Do you agree with me?',
+        'Yes',
+        'No',
+        function okCb() {
+            deleteBlog(id);
+            successMessage("successfully")
+        },
+        function cancelCb() {
+        },
+        {
+        },
+        );
+}
+
+function deleteBlog(id){
     let lstBlogs = getblogs();
 
     const removedBlogs = lstBlogs.filter(x => x.id !== id);
-    setBlogs(removedBlogs);;
+    setBlogs(removedBlogs);
+    read();
 }
 
 function uuidv4() {
@@ -93,6 +189,7 @@ function getblogs (){
    }
 
    return lstBlogs;
+
 }
 
 function setBlogs(blogs){
@@ -102,4 +199,22 @@ function setBlogs(blogs){
 
 }
 
-run();
+$("#save_btn").click(() => {
+    const title = $("#title").val();
+    const author = $("#author").val();
+    const content = $("#content").val();
+
+    if(_blogId){
+        update(_blogId, title, author, content);
+    }else{
+        create(title, author, content)
+    }
+
+    $("#title").val("");
+    $("#author").val("");
+    $("#content").val("");
+
+    $("#title").focus();
+
+    read();
+})
