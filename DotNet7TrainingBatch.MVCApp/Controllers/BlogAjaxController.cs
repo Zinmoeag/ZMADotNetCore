07670911a38213bc27d70d02f26d61fe4902fdf7
Ast_Zinmoag <zinmoagdev@gmail.com>
2024-03-19 19:18:24 +0630

    fdd

---
+++ DotNet7TrainingBatch.MVCApp/Controllers/BlogAjaxController.cs
@@ -0,0 +1,90 @@
+﻿using DotNet7TrainingBatch.MVCApp;
+using DotNet7TrainingBatch.MVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet7TrainingBatch.MVCApp.Controllers
+{
+    public class BlogAjaxController : Controller
+    {
+        private readonly AppDbContext _db;
+        public BlogAjaxController()
+        {
+            _db = new AppDbContext();
+        }
+
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            List<BlogModel> lst = _db.Blogs.ToList();
+
+            return View(lst);
+        }
+
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            BlogModel blog = _db.Blogs.FirstOrDefault(item => item.blog_id == id);
+
+            if(blog is null)
+            {
+                return Redirect("Blog");
+            }
+
+            return View("BlogAjaxEdit", blog);
+
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogAjaxCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogModel blog)
+        {
+            _db.Blogs.Add(blog);
+            _db.SaveChanges();
+
+            return Json("Index");
+
+            ddd
+        }
+
+        [HttpPut]
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id, BlogModel blog)
+        {
+            BlogModel item = _db.Blogs.FirstOrDefault(item => item.blog_id == id);
+
+            if(item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            item.blog_title = blog.blog_title;
+            item.blog_author = blog.blog_author;
+            item.blog_content = blog.blog_content;
+            _db.SaveChanges();
+
+            return Redirect("/Blog");
+
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeleteBlog(int id)
+        {
+            BlogModel blog = _db.Blogs.FirstOrDefault(item => item.blog_id == id);
+
+            if(blog is null) {
+                return Redirect("/Blog");
+            }
+
+            _db.Remove(blog);
+            _db.SaveChanges();
+            return Redirect("/Blog");
+        }
+    }
+}
